c67cbe619debc7ad8414fe14424b629090231a5d
Jiabao_Tian <jiabao_tian@163.com>
2021-04-30 14:12:58 +0800

    2weeks2session

--- parse.js
+++ parse.js
@@ -1,3 +1,15 @@
-module.exports.parserHtml  = function parserHtml() {
+const EOF = Symbol('EOF')
+
+function data(c){
+
+}
+
+module.exports.parserHtml  = function parserHtml(html) {
     console.log(parserHtml);
+    let state = data;
+    // 状态机
+    for(let c of html){
+        state = state(c);
+    }
+    state = state(EOF);
 }
